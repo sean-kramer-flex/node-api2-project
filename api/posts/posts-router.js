b28c503934fb5cbd12532fab833ed3d671149e35
Sean Kramer <seandominickramer@gmail.com>
2021-02-12 15:46:06 -0500

    Add endpoint to get comments for a post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -49,14 +49,14 @@ router.post('/posts', (req, res) => {
 
 //endpoint to edit a post
 router.put('/posts/:id', (req, res) => {
-  if (!req.params.id) {
-    return res.status(404).json({ message: "The post with the specified ID does not exist" })
-  }
 
   if(!req.body.title || !req.body.contents) {
     return res.status(400).json({ message: "Please provide title and contents for the post" })
   }
 
+  posts.findById(req.params.id)
+  .then((post) => {
+if (post) {
   posts.update(req.params.id, req.body)
   .then((post) => {
     console.log(post)
@@ -66,21 +66,54 @@ router.put('/posts/:id', (req, res) => {
     console.log(err)
     res.status(500).json({ message: "The post information could not be modified" })
   })
+} else {
+  res.status(404).json({ message: "The post with the specified ID does not exist" })
+}
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json({ message: "The post information could not be modified" })
+  })
 })
 
 //endpoint to delete a post
 router.delete('/posts/:id', (req, res) => {
-  if (!req.params.id) {
-    return res.status(404).json({ message: "The post with the specified ID does not exist" })
-  }
-
-  posts.remove(req.params.id)
+  posts.findById(req.params.id)
   .then ((post) => {
-    res.status(200).json({message: "Post removed"})
-  })
+    if (post) {
+      posts.remove(req.params.id)
+      .then ((post) => {
+        res.status(200).json({message: "Post removed"})
+      })
+      .catch((err) => {
+        console.log(err)
+        res.status(500).json({ message: "The post could not be removed" })
+      })
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist" })
+    }
+    })
   .catch((err) => {
-    console.log(err)
-    res.status(500).json({ message: "The post could not be removed" })
+      console.log(err)
+      res.status(500).json({ message: "The post could not be removed" })
+    })
+  })
+
+  
+
+//endpoint to get comments for a post
+router.get('/posts/:id/comments', (req, res) => {
+  posts.findById(req.params.id)
+  .then ((post) => {
+    if (post) {
+posts.findPostComments(req.params.id)
+.then ((comments) => {
+  res.status(200).json(comments)
+})
+.catch((err) => {console.log(err);})
+    } else {
+      res.status(404).json({ message: "The post with the specified ID does not exist" })
+    }
   })
 })
 
