2805161cadac98d944f2c799ee7c4104c762bce5
Sean Kramer <seandominickramer@gmail.com>
2021-02-12 15:02:10 -0500

    Add endpoint to edit a post

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -47,5 +47,26 @@ router.post('/posts', (req, res) => {
   })
 })
 
+//endpoint to edit a post
+router.put('/posts/:id', (req, res) => {
+  if (!req.params.id) {
+    return res.status(404).json({ message: "The post with the specified ID does not exist" })
+  }
+
+  if(!req.body.title || !req.body.contents) {
+    return res.status(400).json({ message: "Please provide title and contents for the post" })
+  }
+
+  posts.update(req.params.id, req.body)
+  .then((post) => {
+    console.log(post)
+    res.status(200).json(post)
+  })
+  .catch((err) => {
+    console.log(err)
+    res.status(500).json({ message: "The post information could not be modified" })
+  })
+})
+
 
 module.exports = router
