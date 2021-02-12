646c215caaaa7e6b84b7f975c8a8827a4e68a9e1
Sean Kramer <seandominickramer@gmail.com>
2021-02-12 14:00:34 -0500

    Set up basic server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const server = require('./api/server')
+
+server.get('/', (req, res) => {
+  res.json("server working!")
+})
+
+server.listen(4000, () => {
+  console.log('server listening at http://localhost:4000');
+})
